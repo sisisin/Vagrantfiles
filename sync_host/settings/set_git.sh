c8ce2c7f6803410ca1785c782a3241cb9b810660
sisisin <azsisisin@gmail.com>
2015-05-17 12:02:48 +0900

    fix set_git.sh

--- sync_host/settings/set_git.sh
+++ sync_host/settings/set_git.sh
@@ -5,9 +5,8 @@ cp /sync_guest/settings/.gitconfig /home/vagrant/
 
 cd /home/vagrant/.ssh/
 ssh-keygen -t rsa -C "azsisisin@gmail.com"  -N "" -f github_id_rsa
-eval `ssh-agent`
-ssh-add /home/vagrant/.ssh/github_id_rsa
 
-echo eval `ssh-agent` >> /home/vagrant/.bashrc
+echo eval \`ssh-agent\` >> /home/vagrant/.bashrc
 echo ssh-add /home/vagrant/.ssh/github_id_rsa >> /home/vagrant/.bashrc
 
+chown -R vagrant:vagrant /home/vagrant/.ssh/github_id_rsa*
