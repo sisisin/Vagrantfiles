631ff6bd5f03878b3294d8093fa9616f623dcef1
sisisin <azsisisin@gmail.com>
2015-05-18 21:52:49 +0900

    add Vagrantfile and change path 'mariadb.repo'

--- sync_host/settings/ins_maria10.sh
+++ sync_host/settings/ins_maria10.sh
@@ -1,4 +1,4 @@
 rpm --import https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
-cp /vagrant/mariadb.repo /etc/yum.repos.d/mariadb.repo
+cp /sync_guest/settings/mariadb.repo /etc/yum.repos.d/mariadb.repo
 rpm -e --nodeps mariadb-libs
 yum -y install MariaDB-client MariaDB-server
