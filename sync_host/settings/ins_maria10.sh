cb7a1485ff2f17b5415cdcf0980f742591247393
sisisin <azsisisin@gmail.com>
2015-05-22 20:47:18 +0900

    add mysql provision

--- sync_host/settings/ins_maria10.sh
+++ sync_host/settings/ins_maria10.sh
@@ -2,3 +2,14 @@ rpm --import https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 cp /sync_guest/settings/mariadb.repo /etc/yum.repos.d/mariadb.repo
 rpm -e --nodeps mariadb-libs
 yum -y install MariaDB-client MariaDB-server
+
+firewall-cmd --permanent --add-port=3306/tcp
+
+
+#root password
+/usr/bin/mysqladmin -u root password ''
+
+
+#setting utf8
+
+sed -i -e "s/\[mysqld\]/[mysqld]\ncharacter-set-server=utf8\nplugin-load=handlersocket.so/" /etc/my.cnf.d/server.cnf
